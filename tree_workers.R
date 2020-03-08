616d5b99f4a2a75994dab9f7f1a0a44330b0293d
Andreas Cardeneo <andreas@cardeneo.de>
2020-03-08 22:32:35 +0000

    ENH: tree helpers, in script and in markdown

--- tree_workers.R
+++ tree_workers.R
@@ -20,3 +20,21 @@ all_children_of_node <- function(tree_frame, node_num) {
 }
 
 
+#' Join confusion information to data.frame with tree node data
+#'
+#' The following function expects a `data.frame` with at least these three columns:
+#' C: the class
+#' P: the number of positive instances
+#' N: the number of negative instances
+#' 
+#' @param treeframe 
+#'
+#' @return the \code{data.frame} with confusion information in columns TP, TN, FP, FN
+#' @export
+#'
+join_confusion_info <- function(treeframe) {
+  treeframe %>% mutate(TP = if_else(C == 1, P, 0), 
+                       TN = if_else(C == 1, 0, N), 
+                       FP = if_else(C == 1, N, 0),
+                       FN = if_else(C == 1, 0, P))  
+}
